de4f552e4ab34bb34753472605c5b4772caaf518
AndresEfQ <andresf2641@gmail.com>
2023-08-05 01:05:14 -0500

    Complete tasks 01-03

---
+++ src/clean-code/03-functions.ts
@@ -0,0 +1,43 @@
+(() => {
+
+  function getMovieById( id: string ) {
+      console.log({ id });
+  }
+
+  function getMovieCastById( id: string ) {
+      console.log({ id });
+  }
+
+  function getActorBioById( id: string ) {
+      console.log({ id });
+  }
+  
+  // Crear una película
+  interface Movie {
+    cast:         string[];
+    description:  string;
+    rating:       number;
+    title:        string;
+  }
+
+  function createMovie({ cast, description, rating, title }: Movie ) {
+      console.log({ title, description, rating, cast });
+  }
+
+  // Crea un nuevo actor
+  function createNewActor( fullName: string, birthdate: Date ): boolean {
+      
+      // tarea asincrona para verificar nombre
+      // ..
+      // ..
+      if ( fullName === 'fernando' ) return false;
+
+      console.log('Crear actor');
+      return true;        
+
+  }
+
+  
+
+
+})();
